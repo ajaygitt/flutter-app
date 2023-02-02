3309f69e5435aaa495d547e69aa0ee3cef3bb792
ajaypradeep <ajaypradeep1712@gmail.com>
2023-02-02 22:15:23 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:goodfood/presentation/components/bottom_nav/bottom_nav.dart';
 import 'package:goodfood/presentation/pages/Main_page/main_page.dart';
+import 'package:goodfood/presentation/pages/SplashScreen/SplashScreen.dart';
 
 
 void main() {
@@ -17,7 +18,7 @@ class MyWidget extends StatelessWidget {
     return MaterialApp(
       title: 'FlutterApp',
       theme: ThemeData(primarySwatch: Colors.red),
-      home: MainPage()
+      home: SplashScreen()
     );
   }
 }
---
+++ lib/presentation/pages/SplashScreen/SplashScreen.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:goodfood/presentation/pages/Main_page/main_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 10), () {
+      print("jhelo");
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => MainPage()));
+    });
+
+    print('hello');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Container(
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                  image: AssetImage('assets/images/loadingLogo.png')),
+              CircularProgressIndicator(),
+            ],
+          )),
+        ));
+  }
+}
